45ffc5d0b0b2ad30a9b28ab4b09a9d0dad7017ff
ZANE <130233619+chochozane@users.noreply.github.com>
2023-12-27 16:52:23 +0900

    [ADD] Animations and Script
    add Animations and AnimationsController cs

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private PlayerInputController controller;
+
+    [SerializeField] private Animator anim;
+
+    private void Awake()
+    {
+        controller = GetComponent<PlayerInputController>();
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller.OnMoveEvent += AnimState;
+    }
+
+    private void AnimState(Vector2 direction)
+    {
+        anim.SetBool("Move", direction.magnitude > 0f);
+    }
+}
--- Assets/Scripts/TopDownAimRotation.cs
+++ Assets/Scripts/TopDownAimRotation.cs
@@ -13,7 +13,7 @@ public class TopDownAimRotation : MonoBehaviour
         _controller = GetComponent<TopDownCharacterController>();
     }
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _controller.OnLookEvent += OnAim;
     }
