9ec4369a7824e23936fff6bca7b5830f59ef7341
ZANE <130233619+chochozane@users.noreply.github.com>
2023-12-28 20:49:11 +0900

    [EDIT] Script - TopDownMovement
    add Knockback

--- Assets/Scripts/TopDownMovement.cs
+++ Assets/Scripts/TopDownMovement.cs
@@ -7,16 +7,17 @@ public class TopDownMovement : MonoBehaviour
     private CharacterStatsHandler _stats;
 
     [SerializeField] private SpriteRenderer characterRenderer;
-    private SpriteRenderer _characterWeaponRenderer;
+    [SerializeField] private SpriteRenderer _characterWeaponRenderer;
 
     private Vector2 _movementDirection = Vector2.zero; // 이걸 활용해서 이동 구현 !
+    private Vector2 _knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
 
     private void Awake()
     {
         _controller = GetComponent<TopDownCharacterController>();
         _rigidbody = GetComponent<Rigidbody2D>();
         _stats = GetComponent<CharacterStatsHandler>();
-        _characterWeaponRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void Start()
@@ -32,11 +33,28 @@ public class TopDownMovement : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovement(_movementDirection);
+
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime; // deltaTime 은 Update 에서 사용, FixedUpdate 에서는 fixedDeltaTime 사용 !
+        }
+    }
+
+    private void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        _knockback = -(other.position - transform.position).normalized * power;
     }
 
     private void ApplyMovement(Vector2 direction)
     {
         direction *= _stats.CurrentStats.speed; // 속도 up
+
+        if (knockbackDuration > 0.0f)
+        {
+            direction += _knockback;
+        }
+
         _rigidbody.velocity = direction;
     }
 
@@ -46,6 +64,13 @@ public class TopDownMovement : MonoBehaviour
         {
             characterRenderer.flipX = true;
             _characterWeaponRenderer.flipX = true;
+
+            // todo 무기의 x좌표도 flip 시키고 싶은데..
+            Vector3 newPos = new Vector3(-0.5f, -0.04f, 0f);
+            _characterWeaponRenderer.transform.position = newPos;
+
+            
+
         }
 
         if (_movementDirection.x >= 0f)
